d724aee3b9321c6aa252435919df7de3ffd8d451
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-05 14:57:35 +0900

    Title: 다양하게 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240605/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Total days in Year" << "\n" 
+    << "365" << "\n" << "Circumference rate" << "\n" << "3.1415926535" << endl;
+
+    return 0;
+}
