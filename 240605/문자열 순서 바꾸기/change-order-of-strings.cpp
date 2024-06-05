f117ec4194dc77c025d5e833abc04fd4869a3958
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-05 15:19:45 +0900

    Title: 문자열 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240605/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str1, str2;
+
+    cin >> str1 >> str2;
+    cout << str2 << "\n" << str1;
+
+    return 0;
+}
