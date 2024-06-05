6e270257e313849edf506bcf85ad512e88f6ce87
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-05 15:12:02 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240605/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 1, b = 2, c = 3;
+    a+= (b+c);
+    b = a;
+    c = a;
+    cout << a << " " << b << " " << c;
+
+    return 0;
+}
