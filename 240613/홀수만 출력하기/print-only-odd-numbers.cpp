e21fbd08b8a6f684eaf2ca46a5ac8e54c4d9c154
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-13 13:00:09 +0900

    Title: 홀수만 출력하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240613/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	
+	int n;
+
+	cin >> n;
+	vector<int> vec(n);
+
+	for (int i = 0; i < vec.size(); i++)
+		cin >> vec[i];
+	
+	for (int num : vec) {
+		if (num % 3 == 0)
+			cout << num << endl;
+	}
+
+	return 0;
+}
