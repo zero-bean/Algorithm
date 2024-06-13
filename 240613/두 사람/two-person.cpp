a451ab4034ccc4cfbee7828299d5a00268c1b92e
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-13 11:39:21 +0900

    Title: 두 사람, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240613/두 사람/two-person.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	char c, d;
+
+	cin >> a >> c >> b >> d;
+
+	if ((a >= 19 && c == 'M') || (b >= 19 && d == 'M'))
+		cout << 1;
+	else
+		cout << 0;
+
+	return 0;
+}
