ea2b770cec090f43a8ec45e1b6cada95792be7a0
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-13 12:47:11 +0900

    Title: 홀수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240613/홀수만 출력 2/output-only-odd-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cin >> b >> a;
+
+	for (int i = b; i >= a; i--) {
+		if (i % 2 != 0)
+			cout << i << " ";
+	}
+	
+	return 0;
+}
