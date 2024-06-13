e0f7db3c665f98a794f6bca542423fdf1e915743
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-13 11:00:15 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240613/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cin >> a >> b;
+
+	int num1 = a + b;
+	int num2 = a - b;
+	double result = (double)num1 / (double)num2;
+	cout.fixed;
+	cout.precision(3);
+	cout << result;
+
+	return 0;
+}
