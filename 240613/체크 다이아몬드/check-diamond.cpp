163a7f8371cd1172974f9793aea11f8d1106f96c
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-13 23:27:23 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240613/체크 다이아몬드/check-diamond.cpp
+++ 240613/체크 다이아몬드/check-diamond.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 void drawDiamond(int size) {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int j = n; j > i + 1; j--)
+        for (int j = size; j > i + 1; j--)
             cout << " ";
         for (int k = 0; k < i + 1; k++)
             cout << "* ";
         cout << endl;
     }
 
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < size; i++)
     {
         for (int j = 0; j < i; j++)
             cout << " ";
-        for (int k = n; k > i; k--)
+        for (int k = size; k > i; k--)
             cout << "* ";
         cout << endl;
     }
@@ -29,4 +29,6 @@ int main()
     cin >> n;
 
     drawDiamond(n);
+
+    return 0;
 }
