a8a84e4cec71c1332fd465d4d300beebb6eea7f8
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-13 12:42:06 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240613/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+
+	cin >> a >> b >> c;
+
+	int mid = a;
+
+	if ((a > b && a < c) || (a > c && a < b))
+		cout << a;
+	else if ((b > a && b < c) || (b > c && b < a))
+		cout << b;
+	else
+		cout << c;
+
+	return 0;
+}
