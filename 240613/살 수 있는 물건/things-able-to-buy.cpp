7e76cc0f75d80d93451d5b466ac5607a60f9132f
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-13 11:11:53 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240613/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+
+	cin >> n;
+
+	if (n >= 3000)
+		cout << "book";
+	else if (n >= 1000)
+		cout << "mask";
+	else
+		cout << "no";
+
+	return 0;
+}
