e3678742f49daa7efb4f849341cbcf8c65a2d54e
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-15 19:55:11 +0900

    Title: 좌우반전시킨 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240615/좌우반전시킨 정사각형/left-right-inverted-square.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for (int i=1; i<=n; i++) {
+        for (int j=0; j<n; j++) {
+            cout << (n - j) * i << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
