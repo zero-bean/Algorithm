b9be0958a5dd32320fd0d8773288988f67d41266
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-15 21:30:26 +0900

    Title: 구구단 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240615/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    for (int i=2; i<=8; i+=2) {
+        for (int j=b; j>=a; j--) {
+            cout << j << " * " << i << " = " << j*i;
+            if (j!=a) {
+                cout << " / ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
