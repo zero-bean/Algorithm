9900a518468e080b4874d818b98b77acd57baf6e
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-15 19:39:24 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240615/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for (int i=n; i>0; i--) {
+        for (int j=n; j>0; j--) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
