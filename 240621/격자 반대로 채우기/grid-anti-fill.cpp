24f922812cec1360a3cc59bd35e5fcc0b25be722
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-21 15:32:33 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240621/격자 반대로 채우기/grid-anti-fill.cpp
+++ 240621/격자 반대로 채우기/grid-anti-fill.cpp
@@ -5,28 +5,24 @@ using namespace std;
 
 int main() {
     int n;
-
     cin >> n;
 
     vector<vector<int>> arr(n, vector<int>(n));
 
-    for (int i=0; i<arr.size(); i+=2) {
-        for (int j=0; j<arr[i].size(); j++)
-            arr[j][i] =  (n-i-1)*n + j +1 ;
-    }
-
-    for (int i=1; i<arr.size(); i+=2) {
-        for (int j=0; j<arr[i].size(); j++)
-            arr[j][i] =  (n-i)*n - j;
-    }
-
-    for (int i=0; i<arr.size(); i++) {
-        for (int j=0; j<arr[i].size(); j++) {
-            cout << arr[i][j] << " ";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i % 2 == 0)
+                arr[j][i] = ((n - i - 1) * n) + (j + 1);
+            else
+                arr[j][i] = ((n - i) * n) - j;
         }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << arr[i][j] << " ";
         cout << endl;
     }
-
 
     return 0;
 }
