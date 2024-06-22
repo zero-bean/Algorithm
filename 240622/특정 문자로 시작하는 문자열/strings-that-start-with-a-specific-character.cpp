ce0c3492fa46cbced5c1adf0aff0bab3af7dfdf0
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-22 23:06:57 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240622/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n, count = 0;
+    float avg = 0;
+    char c;
+
+    cin >> n;
+    cin.ignore();
+
+    vector<string> vec(n);
+    for (string& s : vec)
+        getline(cin, s);
+
+    cin.get(c);
+
+    for (string& s : vec) {
+        if (s[0] == 'c') {
+            avg += s.length();
+            count++;
+        }
+    }
+
+    cout << fixed;
+    cout.precision(2);
+    cout << count << " " << avg / count;
+
+    return 0;
+}
