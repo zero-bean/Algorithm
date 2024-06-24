67625c6c228755f98e1bd4694815008fd24e5353
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-24 14:31:28 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240624/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
+++ 240624/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     string str[2];
     
-    for (string s : str)
+    for (string& s : str)
         getline(cin, s);
 
     str[0] + str[1] == str[1] + str[0] ? cout << "true" : cout << "false";
