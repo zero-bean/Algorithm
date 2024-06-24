22473f99884efac1464008fcd99171a1ce28f657
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-24 15:02:03 +0900

    Title: 부분 문자열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240624/부분 문자열의 개수/number-of-substrings.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void countSubsequence(const vector<string>& v) {
+    int count = 0;
+
+    for (int i=0; i<v[0].size() - v[1].size() - 1; i++) {
+        if (v[0].substr(i, v[1].size()).compare(v[1]))
+            count++;
+    }
+
+    cout << count;
+}
+
+int main() {
+    vector<string> vec(2);
+
+    for (string& s : vec)
+        getline(cin, s);
+
+    countSubsequence(vec);
+
+    return 0;
+}
