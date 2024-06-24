8c3ac0cdfbf11bfe5a7d445c4bc3f21a594cc357
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-06-24 14:17:24 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240624/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str[2];
+    
+    for (string s : str)
+        getline(cin, s);
+
+    str[0] + str[1] == str[1] + str[0] ? cout << "true" : cout << "false";
+
+    return 0;
+}
