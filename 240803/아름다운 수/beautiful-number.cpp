52e19e05f9893c09360f7cba387b6fdd0712e5d8
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-08-03 23:27:46 +0900

    Title: 아름다운 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240803/아름다운 수/beautiful-number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    vector<int> dp(n);
+
+    dp[0] = 1;
+    dp[1] = 2;
+    dp[2] = 4;
+    dp[3] = 8;
+
+    for (int i = 3; i < n; i++)
+        dp[i] = dp[i-1] + dp[i-2];
+
+    cout << dp[n-1];
+
+    return 0;
+}
