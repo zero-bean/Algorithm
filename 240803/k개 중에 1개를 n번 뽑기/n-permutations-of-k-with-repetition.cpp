f25a7fa1c0237942a12419d4b6dd6370ab6214d6
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-08-03 16:14:29 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240803/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printResult(const vector<int>& v) {
+    for (const int& i : v)
+        cout << i << " ";
+    cout << endl;
+
+    return;
+}
+
+void recursive(vector<int> v, int pos, int k, int n) {
+    if (pos == n) {
+        printResult(v);
+        return;
+    }
+
+    for (int i = 1; i <= k; i++) {
+        v[pos] = i;
+        recursive(v, pos + 1, k, n);
+    }
+}
+
+int main() {
+    int n, k;
+
+    cin >> n >> k;
+
+    vector<int> v(n);
+
+    recursive(v, 0, k, n);
+
+    return 0;
+}
