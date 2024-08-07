69899c0e9e54ceacffcb960f392c58245137fede
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-08-07 19:34:54 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240807/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool in_Range(pair<int, int> pos, pair<int, int> max) {
+    if (pos.first < 0 || pos.first >= max.first || pos.second < 0 || pos.second >= max.second)
+        return false;
+
+    return true;
+}
+
+bool can_Go(const vector<vector<bool>>& v, pair<int, int> pos) {
+    if (!in_Range(pos, {v.size(), v[0].size()}))
+        return false;
+
+    if (v[pos.first][pos.second])
+        return false;
+    
+    return true;
+}
+
+void dfs(vector<vector<bool>>& v, pair<int, int> pos) {
+    int dx[2] = {0, 1};
+    int dy[2] = {-1, 0};
+
+    for (int i=0; i<2; i++) {
+        int nx = pos.first + dx[i];
+        int ny = pos.second + dy[i];
+
+        if (can_Go(v, {nx, ny})) {
+            v[nx][ny] = true;
+            dfs(v, {nx, ny});
+        }
+    }
+}
+
+int main() {
+    int N, M;
+
+    cin >> N >> M;
+
+    vector<vector<bool>> grid(N, vector<bool>(M, false));
+
+    for (vector<bool>& v : grid) {
+        for (bool b : v)
+            cin >> b;
+    }
+
+    dfs(grid, {0, 0});
+
+    cout << grid[N - 1][M - 1];
+
+    return 0;
+}
