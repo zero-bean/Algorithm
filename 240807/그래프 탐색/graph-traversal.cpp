53bd4e53c7d588c063bb23507b126c4abb04ecfe
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-08-07 12:07:44 +0900

    Title: 그래프 탐색, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240807/그래프 탐색/graph-traversal.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void dfs(const vector<vector<int>>& v, vector<bool>& visited, int start, int& count) {
+    visited[start] = true;
+
+    for (int i=0; i<v[start].size(); i++) {
+        int next = v[start][i];
+        if (!visited[next] ) {
+            visited[next] = true;
+            count++;
+            dfs(v, visited, next, count);
+        }
+    }
+}
+
+int main() {
+    int N, M, count = 0;
+
+    cin >> N >> M;
+
+    vector<vector<int>> graph(N+1);
+    vector<bool> visited(N+1, false);
+
+    while (M--) {
+        int a, b;
+
+        cin >> a >> b;
+
+        graph[a].push_back(b);
+    }
+
+    dfs(graph, visited, 1, count);
+
+    cout << count;
+
+    return 0;
+}
