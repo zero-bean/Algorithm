7992c88a82a716ae91e07a1b814c8bf1d2f6c24d
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-08-09 23:36:51 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 442ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240809/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+bool in_Range(int x, int y, pair<int, int> range) {
+    return x >= 0 && x < range.first && y >= 0 && y < range.second;
+}
+
+void bfs(vector<vector<bool>>& grid) {
+    queue<pair<int, int>> q;
+    int dx[4] = { -1,1,0,0 };
+    int dy[4] = { 0,0,1,-1 };
+
+    q.push({ 0,0 });
+
+    while (!q.empty()) {
+        pair<int, int> current_Pos = q.front();
+        
+        q.pop();
+
+        grid[current_Pos.first][current_Pos.second] = false;
+
+        for (int i = 0; i < 4; i++) {
+            pair<int, int> next_Pos = { current_Pos.first + dx[i], current_Pos.second + dy[i] };
+
+            if (in_Range(next_Pos.first, next_Pos.second, { grid.size(), grid[0].size() }) && grid[next_Pos.first][next_Pos.second])
+                q.push(next_Pos);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+
+    cin >> n >> m;
+
+    vector<vector<bool>> grid(n, vector<bool>(m, false));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            bool b;
+
+            cin >> b;
+
+            grid[i][j] = b;
+        }
+    }
+
+    bfs(grid);
+
+    cout << !(grid[n - 1][m - 1]);
+
+    return 0;
+}
