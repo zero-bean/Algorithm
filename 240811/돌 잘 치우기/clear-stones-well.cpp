e5f90f110ff7ae622db65aaee6038e4530518a95
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-08-11 17:34:44 +0900

    Title: 돌 잘 치우기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240811/돌 잘 치우기/clear-stones-well.cpp
+++ 240811/돌 잘 치우기/clear-stones-well.cpp
@@ -27,27 +27,33 @@ bool in_Range(const pair<int, int>& p, const int& n) {
     return p.first >= 0 && p.first < n && p.second >= 0 && p.second < n;
 }
 
-int bfs(vector<vector<int>>& grid, const pair<int, int>& sp) {
+int bfs(vector<vector<int>>& grid, const vector<pair<int, int>>& sp) {
     int n = grid.size();
-    int count = 1;
-    queue<pair<int, int>> q;
+    int count = 0;
 
-    grid[sp.first][sp.second] = 1;
-    q.push(sp);
+    for (const pair<int, int>& p : sp) {
+        if (grid[p.first][p.second])
+            continue;
 
-    while (!q.empty()) {
-        int dx[4] = { -1,1,0,0 };
-        int dy[4] = { 0,0,1,-1 };
-        pair<int, int> curr = q.front();
-        q.pop();
+        grid[p.first][p.second] = 1;
+        count++;
+        queue<pair<int, int>> q;
+        q.push(p);
 
-        for (int i = 0; i < 4; i++) {
-            pair<int, int> next = { curr.first + dx[i], curr.second + dy[i] };
+        while (!q.empty()) {
+            int dx[4] = { -1,1,0,0 };
+            int dy[4] = { 0,0,1,-1 };
+            pair<int, int> curr = q.front();
+            q.pop();
 
-            if (in_Range(next, n) && !grid[next.first][next.second]) {
-                grid[next.first][next.second] = 1;
-                q.push(next);
-                count++;
+            for (int i = 0; i < 4; i++) {
+                pair<int, int> next = { curr.first + dx[i], curr.second + dy[i] };
+
+                if (in_Range(next, n) && !grid[next.first][next.second]) {
+                    grid[next.first][next.second] = 1;
+                    q.push(next);
+                    count++;
+                }
             }
         }
     }
@@ -82,15 +88,13 @@ int main() {
 
     generate_Combination(combinations, obstacles, m);
 
-    for (int i = 0; i < starts.size(); i++) {
-        for (int j = 0; j < combinations.size(); j++) {
-            vector<vector<int>> copy = grid;
+    for (int j = 0; j < combinations.size(); j++) {
+        vector<vector<int>> copy = grid;
 
-            for (const pair<int, int>& p : combinations[j]) 
-                copy[p.first][p.second] = 0;
+        for (const pair<int, int>& p : combinations[j])
+            copy[p.first][p.second] = 0;
 
-            answer = max(answer, bfs(copy, starts[i]));
-        }
+        answer = max(answer, bfs(copy, starts));
     }
 
     cout << answer;
