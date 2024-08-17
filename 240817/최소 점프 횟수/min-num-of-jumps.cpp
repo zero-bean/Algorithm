f5a09c404ddfc900ad0a8eeb1ab61cbf9c04ba9a
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-08-17 19:37:30 +0900

    Title: 최소 점프 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240817/최소 점프 횟수/min-num-of-jumps.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> jump;
+int n, answer = 100;
+
+void find_Minimum_Jump(int idx, int cnt) {
+    if (idx >= n - 1) {
+        answer = min(cnt, answer);
+        return;
+    }
+
+    for (int i=1; i<= jump[idx]; i++) {
+        find_Minimum_Jump(idx + i, cnt + 1);
+    }
+}
+
+void solve() {
+    find_Minimum_Jump(0, 0);
+
+    answer = (answer == 100) ? 100 : answer;
+
+    cout << answer;
+}
+
+int main() {
+    cin >> n;
+
+    jump.resize(n);
+    for(int& j : jump)
+        cin >> j;
+
+    solve();
+
+    return 0;
+}
