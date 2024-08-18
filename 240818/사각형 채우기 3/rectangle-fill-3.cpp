11396abb91b82759a35b5fb2449c12bd0851a49e
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-08-18 16:13:46 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240818/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> dp;
+int n;
+
+int main() {
+    cin >> n;
+
+    dp.resize(n+1);
+    dp[0] = 1;
+    dp[1] = 2;
+    dp[2] = 7;
+    dp[3] = 22;
+
+    for (int i=4; i<=n; i++)
+        dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % 10007;
+
+    cout << dp[n];
+
+    return 0;
+}
