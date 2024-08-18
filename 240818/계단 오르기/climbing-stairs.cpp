541a029f4c678fd049e1845b58b97beee60f021e
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-08-18 13:04:24 +0900

    Title: 계단 오르기, Time: 1087ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240818/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> dp;
+int n;
+
+int up_Stairs(int n) {
+    if (n <= 3)
+        return dp[n];
+
+    dp[n] = (up_Stairs(n - 2) + up_Stairs(n - 3)) % 10007;
+
+    return dp[n];
+}
+
+int main() {
+    cin >> n;
+
+    dp.resize(n + 1, 0);
+    dp[2] = 1;
+    dp[3] = 1;
+
+    cout << up_Stairs(n);
+
+    return 0;
+}
