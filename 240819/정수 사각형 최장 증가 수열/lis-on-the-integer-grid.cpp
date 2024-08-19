8c90523293f6d5b287cb94dc6adef0d2e0cc6c58
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-08-19 17:25:58 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 128ms, Memory: 2MB, Status: Passed - CodeTree

--- 240819/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 240819/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -1,56 +1,70 @@
 #include <iostream>
+#include <algorithm>
+#include <tuple>
 #include <vector>
+
+#define MAX_N 500
+#define DIR_NUM 4
 
 using namespace std;
 
-vector<vector<int>> grid;
-vector<vector<int>> v;
-int n, ans = 1;
+// 변수 선언
+int n;
+int grid[MAX_N][MAX_N];
+int dp[MAX_N][MAX_N];
 
-bool can_Go(int x, int y) {
-    return (x >= 0 && x < n && y >= 0 && y < n);
+bool InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-void find_Max() {
-    int dx[4] = { -1,1,0,0 };
-    int dy[4] = { 0,0,1,-1 };
+// (x, y)에서 출발하여 조건을 만족하며
+// 도달할 수 있는 칸의 수 중
+// 최대 칸의 수를 구합니다.
+int FindMax(int x, int y) {
+    // 이미 계산해본적이 있다면
+    // 그 값을 바로 반환합니다.
+    if(dp[x][y] != -1)
+        return dp[x][y];
 
-    for (int k = 0; k < n; k++) {
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < 4; i++) {
-                int nx = k + dx[i];
-                int ny = j + dy[i];
+    // 기본값은 자기자신이 됩니다.
+    int best = 1;
 
-                if (can_Go(nx, ny) && grid[nx][ny] > grid[k][j]) {
-                    v[nx][ny] = max(1 + v[k][j], v[nx][ny]);
-                    ans = max(ans, v[nx][ny]);
-                }
-            }
+    // 정수값이 작은 칸부터 순서대로 보며
+    // 4방향에 대해 최적의 칸 수를 계산합니다.
+    int dx[DIR_NUM] = {-1, 1,  0, 0};
+    int dy[DIR_NUM] = { 0, 0, -1, 1};
 
-        }
+    for(int j = 0; j < DIR_NUM; j++) {
+        int nx = x + dx[j], ny = y + dy[j];
+        if(InRange(nx, ny) && grid[nx][ny] > grid[x][y])
+            best = max(best, FindMax(nx, ny) + 1);
     }
 
-}
-
-void solve() {
-    v.resize(n, vector<int>(n, 1));
-
-    find_Max();
+    return dp[x][y] = best;
 }
 
 int main() {
+    // 입력
     cin >> n;
 
-    grid.resize(n, vector<int>(n));
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> grid[i][j];
 
-    for (vector<int>& g : grid) {
-        for (int& i : g)
-            cin >> i;
-    }
+    // 메모이제이션을 위해 
+    // 전부 초기값을 -1로 셋팅해줍니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            dp[i][j] = -1;
 
-    solve();
-
+    // 각 칸에 시작했을 떄
+    // 최대로 이동할 수 있는 칸의 수 중 
+    // 최댓값을 갱신합니다.
+    int ans = 0;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            ans = max(ans, FindMax(i, j));
+    
     cout << ans;
-
     return 0;
 }
