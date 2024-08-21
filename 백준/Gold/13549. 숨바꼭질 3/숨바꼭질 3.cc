cc9840b88017e34cc7831d4c69049dbc49692f4b
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-08-21 14:31:36 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+#include <limits>
+#include <vector>
+
+using namespace std;
+
+int n, k;
+
+bool in_Range(int x) {
+    return x >= 0 && x <= 100000;
+}
+
+void bfs() {
+    vector<bool> visited(100001, false);
+    vector<int> trials(100001, numeric_limits<int>::max());
+    queue<int> q;
+
+    q.push(n);
+    trials[n] = 0;
+
+    while (!q.empty()) {
+        int curr = q.front();
+        q.pop();
+
+        if (curr == k)
+            break;
+
+        if (visited[curr])
+            continue;
+
+        visited[curr] = true;
+
+        int next[3] = { curr * 2, curr + 1, curr - 1 };
+        int plus[3] = { 0,1,1 };
+
+        for (int i = 0; i < 3; i++) {
+            if (in_Range(next[i]) && trials[next[i]] > trials[curr] + plus[i]) {
+                trials[next[i]] = trials[curr] + plus[i];
+                q.push(next[i]);
+            }
+        }
+    }
+
+    cout << trials[k] << endl;
+}
+
+int main() {
+    cin >> n >> k;
+
+    bfs();
+
+    return 0;
+}
