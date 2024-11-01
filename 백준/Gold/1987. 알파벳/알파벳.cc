e9496c34147d0a9dfc749f4aa38a3f3a16c3f1e4
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2024-11-01 22:26:39 +0900

    [Gold IV] Title: 알파벳, Time: 944 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -9,7 +9,7 @@ int R, C;
 
 bool In_Range(const pair<int, int>& p) { return p.first >= 0 && p.first < R && p.second >= 0 && p.second < C; }
 
-void DFS(const pair<int, int>& p, vector<vector<bool>>& visited, vector<bool>& alphabet, int& ans, int trial) {
+void DFS(const pair<int, int>& p, vector<bool>& alphabet, int& ans, int trial) {
 	ans = max(ans, trial);
 
 	int dy[4] = { 0,0,1,-1 };
@@ -20,25 +20,21 @@ void DFS(const pair<int, int>& p, vector<vector<bool>>& visited, vector<bool>& a
 
 		if (!In_Range(nxt))
 			continue;
-		if (visited[nxt.first][nxt.second] || alphabet[grid[nxt.first][nxt.second] - 65])
+		if (alphabet[grid[nxt.first][nxt.second] - 65])
 			continue;
 
-		visited[nxt.first][nxt.second] = true;
 		alphabet[grid[nxt.first][nxt.second] - 65] = true;
-		DFS(nxt, visited, alphabet, ans, trial + 1);
-		visited[nxt.first][nxt.second] = false;
+		DFS(nxt, alphabet, ans, trial + 1);
 		alphabet[grid[nxt.first][nxt.second] - 65] = false;
 	}
 }
 
 void Solve() {
 	vector<bool> alphabet(26, false);
-	vector<vector<bool>> visited(R, vector<bool>(C, false));
 	int answer = 0;
 
-	visited[0][0] = true;
 	alphabet[grid[0][0] - 65] = true;
-	DFS({0,0}, visited, alphabet, answer, 1);
+	DFS({0,0}, alphabet, answer, 1);
 
 	cout << answer << endl;
 }
