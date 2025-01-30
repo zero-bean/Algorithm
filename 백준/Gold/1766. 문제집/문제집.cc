a085f89573de8495c891a7d4bf7349c45a8b5d5a
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2025-01-30 19:24:05 +0900

    [Gold II] Title: 문제집, Time: 48 ms, Memory: 8308 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+vector<set<int>> graph;
+vector<int> orders;
+
+void Solve() {
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int i = 1; i < orders.size(); i++) {
+        if (orders[i] == 0)
+            pq.push(i);
+    }
+
+    while (!pq.empty()) {
+        vector<int> toErase;
+        int curr = pq.top();
+        pq.pop();
+
+        cout << curr << " ";
+
+        for (const auto& a : graph[curr]) {
+            orders[a]--;
+
+            if (orders[a] == 0) {
+                toErase.push_back(a);
+                pq.push(a);
+            }
+        }
+
+        for (const auto& a : toErase) {
+            graph[curr].erase(a);
+        }
+
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, M;
+
+    cin >> N >> M;
+
+    orders = vector<int>(N + 1, 0);
+    graph = vector<set<int>>(N + 1);
+
+    while (M--) {
+        int sp, lp;
+
+        cin >> sp >> lp;
+
+        orders[lp]++;
+        graph[sp].insert(lp);
+    }
+
+    Solve();
+
+    return 0;
+}
