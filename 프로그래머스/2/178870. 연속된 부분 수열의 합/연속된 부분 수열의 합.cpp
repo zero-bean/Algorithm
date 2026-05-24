dbe5d034e37123981924d7436c3001ee29e409c3
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2026-05-24 13:50:58 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 76.66 ms, Memory: 93.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> sequence, int k) 
+{
+    // 초기화
+    const int arrSize = sequence.size();
+    vector<int> prefixSum(arrSize+1, 0);
+    prefixSum[1] = sequence[0];
+    for (int i=1; i<=arrSize; ++i)
+    {
+        prefixSum[i] = prefixSum[i-1] + sequence[i-1];
+    }
+    
+    // 길이가 1인 경우
+    for (int i=0; i<arrSize; ++i)
+    {
+        if (sequence[i] == k)
+        {
+            return vector<int>({i, i});
+        }
+    }
+    
+    // 길이가 n인 경우
+    vector<vector<int>> candidates{};
+    int left = 0;
+    int right = 1;
+    while (right <= arrSize)
+    {
+        const int value = prefixSum[right] - prefixSum[left];
+        
+        if (value == k)
+        {
+            candidates.push_back({left, right - 1});
+            left++;
+            right++;
+        }
+        else if (value > k)
+        {
+            left++;
+        }
+        else
+        {
+            right++;
+        }
+    }
+    
+    sort(candidates.begin(), candidates.end(), 
+         [](const vector<int>& a, const vector<int>& b) 
+         { 
+             if (a.back() - a[0] == b.back() - b[0])
+             {
+                 return a[0] < b[0];
+             }
+             
+             return a.back() - a[0] < b.back() - b[0]; 
+         });
+    return candidates[0];
+}
