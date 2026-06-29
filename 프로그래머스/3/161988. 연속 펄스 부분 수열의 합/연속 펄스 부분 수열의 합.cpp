e46d88b0071e89e062642e4eedf565b2a764b17b
YEONGBIN PAK <58154776+zero-bean@users.noreply.github.com>
2026-06-29 16:11:56 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 13.72 ms, Memory: 24.5 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
@@ -27,11 +27,13 @@ long long calculateSums(const vector<int>& vec)
     long long tmpValue = 0;
     for (int i = 0; i < length; ++i)
     {     
+        // 검사할 원소가 증가하는 경우는 Best Case이므로, 더하고 maxValue를 갱신하기만 하면 된다.
         if (vec[i] >= 0)
         {
             tmpValue += vec[i];
             maxValue = max(tmpValue, maxValue);
         }
+        // 검사할 원소가 감소하는 경우라면 경우의 수를 고려해야 한다.
         else
         {
             // 현재 원소를 넣었을 때 총 값이 0 미만이면, 새로하는 것이 베스트임
@@ -41,6 +43,7 @@ long long calculateSums(const vector<int>& vec)
                 tmpValue = 0;
                 continue;
             }
+            // 아직은 0보단 크므로, 초기화하여 0부터 시도하는 것보단 N부터 계속 시도하는 것이 낫다.
             else
             {
                 tmpValue += vec[i];
